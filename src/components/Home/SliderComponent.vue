f957cbd549b33f9082623ce82c829b4c0825e26e
Kauã Fernandes <kauafernandes.smelo@gmail.com>
2023-10-07 17:53:16 -0300

    Favico

---
+++ src/components/Home/SliderComponent.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="carousel">
+
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'TheSlider',
+
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="home">
     <HomeComponent></HomeComponent>
+    <TheSlider></TheSlider>
   </div>
 </template>
 
 <script>
 
 import HomeComponent from '@/components/Home/HomeComponent.vue';
+import TheSlider from '@/components/Home/SliderComponent.vue';
 
 export default {
   name: 'HomeView',
   components: {
-    HomeComponent
+    HomeComponent,
+    TheSlider
   }
 }
 </script>
