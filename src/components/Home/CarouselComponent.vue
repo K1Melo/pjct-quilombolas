f1b9f20b37e61e750b0cc7bb28baaa4b98033954
Kauã Fernandes <kauafernandes.smelo@gmail.com>
2023-10-14 22:05:09 -0300

    Test without bootstrap

--- src/components/Home/CarouselComponent.vue
+++ src/components/Home/CarouselComponent.vue
@@ -1,4 +1,4 @@
-<!-- <template>
+<template>
     <div class="sliderView">
         <i class="material-icons" id="left" @click="changeSlideLeft">arrow_back_ios</i>
         <div class="wrapper">
@@ -15,7 +15,18 @@
                             <p>Originados da resistência africana contra a escravidão, escravizados fugitivos de suas fazendas buscaram liberdade nas áreas isoladas, onde formaram comunidades independentes, se organizando de acordo com suas próprias tradições culturais e sociais.</p>
                         </div>
                     </div>
-
+                    <!-- <div class="circulos">
+                        <div class="circulo2">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="550" height="550" viewBox="0 0 147 147" fill="none">
+                                <circle cx="73.5" cy="73.5" r="73.5" fill="#4A2F2F"/>
+                            </svg>
+                        </div>
+                        <div class="circulo3">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="250" height="250" viewBox="0 0 147 147" fill="none">
+                                <circle cx="73.5" cy="73.5" r="73.5" fill="#4A2F2F"/>
+                            </svg> 
+                        </div>
+                    </div> -->
                 </div>
                 <div class="slides" style="margin-left: -15px;">
                     <div class="img-slide ">
@@ -403,229 +414,4 @@ export default {
     }
 }
 
-</style> -->
-
-<template>
-    <div class="sliderView">
-        <i class="material-icons" id="left" @click="changeSlideLeft">arrow_back_ios</i>
-        <div class="wrapper">
-            <div class="slider snap-inline">
-                <div class="slides">
-                    <div class="img-slide">
-                        <img src="../../assets/slide1-img.svg" alt="">
-                    </div>
-                    <div class="text-slide">
-                        <div class="title">
-                            <h2>Teste</h2>
-                        </div>
-                        <div class="text">
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus tempora inventore ipsum blanditiis quidem suscipit earum voluptate soluta repellat possimus, voluptates neque dicta commodi sed consectetur quisquam omnis fuga accusantium.</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="slides" style="margin-left: -15px;">
-                    <p>2</p>
-                </div>
-                <div class="slides" style="margin-left: -15px;">
-                    <p>3</p>
-                </div>
-            </div>
-        </div>
-        <i class="material-icons" id="rigth" @click="changeSlideRigth">arrow_forward_ios</i>
-    </div>
-</template>
-
-<script>
-
-export default {
-    data(){
-        return {
-            index: 0
-        }
-    }, 
-    methods: {
-        changeSlideRigth() {
-            const slider = document.querySelectorAll(".slides")
-            if(this.index == 1) {
-                slider.forEach((slide) =>{
-                    slide.classList.add("three");
-                    slide.classList.remove("two");
-                    console.log(slide.classList);
-                });
-                this.index++;
-            } else if(this.index == 0) {
-                slider.forEach((slide) =>{
-                    slide.classList.add("two");
-                    slide.classList.remove("three");
-                });
-                this.index++
-            } else {
-                slider.forEach((slide) =>{
-                    slide.classList.remove("three");
-                    slide.classList.remove("two");
-                });
-                this.index = 0;
-            }
-        },
-        
-        changeSlideLeft() {
-            const slider = document.querySelectorAll(".slides")
-            if(this.index == 1) {
-                slider.forEach((slide) =>{
-                    slide.classList.remove("three");
-                    slide.classList.remove("two");
-                });
-                this.index--;
-            } else if(this.index == 0) {
-                slider.forEach((slide) =>{
-                    slide.classList.remove("two");
-                    slide.classList.add("three");
-                });
-                this.index = 2
-            } else {
-                slider.forEach((slide) =>{
-                    slide.classList.remove("three");
-                    slide.classList.add("two");
-                });
-                this.index = 1;
-            }
-        },
-        
-        
-    }
-}
-</script>
-
-<style scoped lang="less">
-.sliderView {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    min-height: 100vh;
-}
-
-.wrapper {
-    width: 100vw;
-    height: 100vh;
-}
-
-#left {
-    position: absolute;
-    left: 0;
-    margin-left: 10px;
-}
-
-#rigth {
-    position: absolute;
-    right: 0;
-}
-
-#left,
-#rigth {
-    font-size: 50px;
-    z-index: 200;
-    cursor: pointer;
-}
-
-.slider {
-    display: flex;
-    grid-gap: 1rem;
-    width: 100%;
-    height: 100vh;
-    overflow-x: hidden;
-
-    .slides {
-        overflow: hidden;
-        min-width: 100%;
-        height: 100%;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        color: #000000;
-        font-size: 2rem;
-        transition: all 1.5s;
-    }
-
-    .two {
-        transform: translate(-100%);
-    }
-
-    .three {
-        transform: translate(-200.1%);
-    }
-}
-
-
-
-.snap-inline {
-    scroll-snap-type: inline mandatory;
-}
-
-.snap-inline>* {
-    scroll-snap-align: start;
-}
-
-.img-slide {
-    display: flex;
-    justify-content: center;
-    background-image: url(../../assets/background-slide1.jpg);
-    background-position: 0%;
-    background-size: cover;
-    width: 50%;
-    img {
-        width: 75vh;
-    }
-}
-
-.text-slide {
-    width: 50%;
-    padding-left: 5%;
-    .title {
-        // width: 50%;
-        color: #000;
-
-        // margin-bottom: 2vh;
-        font-size: 2em;
-        opacity: 0.8;
-    }
-
-    .text {
-        width: 80%;
-        height: 50%;
-    }
-}
-
-
-
-@media only screen and (max-width: 1000px) {
-    .slider {
-        
-        .slides{
-            justify-content: flex-start;
-            align-items: flex-start;
-            flex-direction: column;
-
-        }
-    }
-
-    .img-slide {
-
-        display: flex;
-        justify-content: center;
-        width: 100%;
-    img {
-        width: 40vh;
-    }
-}
-
-}
-@media only screen and (max-width: 800px) {
-    .slider {
-        overflow-x: auto;
-    }
-
-    .material-icons {
-        display: none;
-    }
-}
 </style>
