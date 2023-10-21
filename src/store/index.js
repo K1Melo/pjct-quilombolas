c111eac43aae9970249d9117595830651dcfb7e7
Kauã Fernandes <kauafernandes.smelo@gmail.com>
2023-10-21 15:44:07 -0300

    Functional map

--- src/store/index.js
+++ src/store/index.js
@@ -4,30 +4,140 @@ export default createStore({
   state: {
     estados: [
       {
+        acronym: 'BA',
         name: 'Bahia',
-        description: 'Lorem Ipsun Foda',
-        currentSlide: true
+        population: 397.059,
       },
       {
-        name: 'Mato Grosso',
-        description: 'Lorem Ipsun Foda',
-        currentSlide: false
-      },
-      {
+        acronym: 'MA',
         name: 'Maranhão',
-        description: 'Lorem Ipsun Foda',
-        currentSlide: false
+        population: 269.074,
       },
       {
+        acronym: 'MG',
+        name: 'Minas Gerais',
+        population: 135.310,
+      },
+      {
+        acronym: 'PA',
         name: 'Pará',
-        description: 'Lorem Ipsun Foda',
-        currentSlide: false
+        population: 135.033,
       },
       {
-        name: 'Paraná',
-        description: 'Lorem Ipsun Foda',
-        currentSlide: false
+        acronym: 'PE',
+        name: 'Pernambuco',
+        population: 78.827,
       },
+      {
+        acronym: 'AL',
+        name: 'Alagoas',
+        population: 37.722,
+      },
+      {
+        acronym: 'PI',
+        name: 'Piauí',
+        population: 31.686,
+      },
+      {
+        acronym: 'GO',
+        name: 'Goiás',
+        population: 30.387,
+      },
+      {
+        acronym: 'SE',
+        name: 'Sergipe',
+        population: 28.124,
+      },
+      {
+        acronym: 'CE',
+        name: 'Ceará',
+        population: 23.955,
+      },
+      {
+        acronym: 'RN',
+        name: 'Rio Grande do Norte',
+        population: 22.384,
+      },
+      {
+        acronym: 'RJ',
+        name: 'Rio de Janeiro',
+        population: 20.344,
+      },
+      {
+        acronym: 'RS',
+        name: 'Rio Grande do Sul',
+        population: 17.496,
+      },
+      {
+        acronym: 'PB',
+        name: 'Paraíba',
+        population: 16.584,
+      },
+      {
+        acronym: 'ES',
+        name: 'Espírito Santo',
+        population: 15.652,
+      },
+      {
+        acronym: 'TO',
+        name: 'Tocantins',
+        population: 12.881,
+      },
+      {
+        acronym: 'AP',
+        name: 'Amapá',
+        population: 12.524,
+      },
+      {
+        acronym: 'MT',
+        name: 'Mato Grosso',
+        population: 11.719,
+      },
+      {
+        acronym: 'SP',
+        name: 'São Paulo',
+        population: 10.999,
+      },
+      {
+        acronym: 'PR',
+        name: 'Paraná',
+        population: 7.113,
+      },
+      {
+        acronym: 'SC',
+        name: 'Santa Catarina',
+        population: 4.447,
+      },
+      {
+        acronym: 'RO',
+        name: 'Rondônia',
+        population: 2.926,
+      },
+      {
+        acronym: 'AM',
+        name: 'Amazonas',
+        population: 2.705,
+      },
+      {
+        acronym: 'MS',
+        name: 'Mato Grosso do Sul',
+        population: 2.546,
+      },
+      {
+        acronym: 'DF',
+        name: 'Distrito Federal',
+        population: 305,
+      },
+      {
+        acronym: 'RR',
+        name: 'Roraima',
+        population: 0,
+      },
+      {
+        acronym: 'AC',
+        name: 'Acre',
+        population: 0,
+      }
     ]
   },
   getters: {
