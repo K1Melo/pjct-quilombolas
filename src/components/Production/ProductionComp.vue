e16a4d1c4a472055b58002c90d735f4c70022301
Kauã Fernandes <kauafernandes.smelo@gmail.com>
2023-10-25 21:45:42 -0300

    Production initial page

---
+++ src/components/Production/ProductionComp.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="production-comp">
+        <div class="title">
+            <h2>PRODUÇÃO</h2>
+        </div>
+    </div>
+</template>
+
+<style lang="less" scoped>
+.production-comp {
+    width: 100%;
+    height: 100vh;
+    background-image: url("../../assets/background-production.png");
+    background-position: bottom right;
+    // background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    display: flex;
+    align-items: center;
+}
+
+.title {
+    height: 70vh;
+
+    h2 {
+        display: flex;
+        justify-content: center;
+        position: absolute;
+        align-items: center;
+        font-size: calc(5em + 1vw);
+        letter-spacing: 6px;
+        width: 100%;
+        height: 25%;
+        background-color: #100404d7;
+    }
+}
+
+@media screen and (max-width: 1024px) {
+    .title {
+        height: 70vh;
+
+        h2 {
+            font-size: calc(4em + 1vw);
+        }
+    }
+}
+@media screen and (max-width: 600px) {
+    .title {
+        height: 70vh;
+
+        h2 {
+            font-size: calc(3em + 1vw);
+        }
+    }
+}
+
+@media screen and (max-width: 300px) {
+    .title {
+        height: 70vh;
+    
+        h2 {
+            font-size: calc(2em + 1vw);
+        }
+    }
+
+}
+
+</style>
--- src/views/ProductionView.vue
+++ src/views/ProductionView.vue
@@ -1,8 +1,20 @@
 <template>
     <div class="production" data-aos="fade-in" data-aos-easing="linear" data-aos-duration="1500">
-        <h1>This is a production page</h1>
+        <ProductionComp></ProductionComp>
     </div>
 </template>
+
+<script>
+
+import ProductionComp from "../components/Production/ProductionComp.vue"
+
+export default {
+    components: {
+        ProductionComp
+    }
+}
+
+</script>
 
 <style scoped>
 
