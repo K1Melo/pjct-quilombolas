69519543b4b446dfda12cef27694fe63210f3e0a
Kauã Fernandes <kauafernandes.smelo@gmail.com>
2023-10-28 13:03:01 -0300

    Resolve particles error

--- src/main.js
+++ src/main.js
@@ -3,6 +3,4 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-import Particles from "vue3-particles";
-
-window.app = createApp(App).use(store).use(router).use(Particles).mount('#app')
+window.app = createApp(App).use(store).use(router).mount('#app')
